266319ac6ef8fcadac5ec96949193182aaedae09
Michael Hoyle <mcalebhoyle@gmail.com>
2022-03-24 14:46:02 -0400

    added environment variable

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
