255e1cd0e82a5c39c308a8d8dd9f22c2bc85d519
Michael Hoyle <mcalebhoyle@gmail.com>
2022-03-25 16:54:08 -0400

    got the application working by adding get and post request

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,16 +15,46 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-
-  fs.readFile('./db/db.json', 'utf-8', function (err, data) {
+app.get("/api/notes", (req, res) => {
+  readFileAsync("./db/db.json", "utf-8").then(function(data) {
+    note = [].concat(JSON.parse(data))
+    res.json(note);
   })
 });
 
-app.post("/notes", (req, res) => {
-  
+app.post("/api/notes", (req, res) => {
+  const postingNotes = req.body;
+  readFileAsync("./db/db.json", "utf-8").then(function(data) {
+    const addingNotes = [].concat(JSON.parse(data));
+    postingNotes.id = addingNotes.length + 1 
+    addingNotes.push(postingNotes);
+    return addingNotes;
+  }).then(function(addingNotes) {
+    writeFileAsync("./db/db.json", JSON.stringify(addingNotes))
+    res.json(postingNotes);
+  })
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+  const toDelete = parseInt(req.params.id);
+  readFileAsync("./db/db.json", "utf-8").then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    const newNotes = []
+    for (let i = 0; i < notes.length; i++) {
+      if (toDelete !== notes[i].id) {
+        newNotes.push(notes[i])
+      }
+    }
+    return newNotes
+  }).then(function(notes) {
+    writeFileAsync("./db/db.json", JSON.stringify(notes))
+    res.send("Note Saved!");
+  })
 })
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
