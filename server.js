da326521f783a7c4ed06c5ba1bfa6bfdd2a31c3e
Michael Hoyle <mcalebhoyle@gmail.com>
2022-03-25 15:41:52 -0400

    redid the routes

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
-const express = require('express');
-const path = require('path');
-const html = require('./routes/html')
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
 
-app.use('/', html)
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 
+  fs.readFile('./db/db.json', 'utf-8', function (err, data) {
+  })
+});
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+app.post("/notes", (req, res) => {
+  
 })
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
