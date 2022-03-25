d0fbbfec4fe63325261abc1cae9d77af8d9df276
Michael Hoyle <mcalebhoyle@gmail.com>
2022-03-25 18:46:36 -0400

    finished off readme and heroku deployment

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 #  great-note-taker
+      
+## Description
+
+In this assignment we were tasked with writing out a note taking application. There is a landing page and a link to the notes section. This is achieved through the use of server routing. When the user looks at the notes they have the ability to enter a note title and content. Exsisting notes will be in the left hand side of the page. When the user presses save the notes are then saved in the right hand side and will remain there until the user presses the delete button located on the note. When the user presses the + button at the top of the page, all the text values are cleared out. This is achieved by using get and post requests to post the new data the user enters into some json data that is stored on the server. The get requests are able to pull this data down and display it in the desired location. The delete function loops through every item that is not wanting to be deleted and then pushing them into a new object that is overriding the old one, removing the one that was wanting to be deleted.
+
+## Installation
+      
+To install necessary components, run the following command:
+
+    npm install  
+            
+## Git Repository 
+
+The GitHub repository can be found [here.](https://github.com/choyle-01/great-note-taker)
+
+## Deployed
+
+The deployed link can be found [here.](https://polar-hollows-30972.herokuapp.com/)
+
+## Visuals 
+
+The link to a video demonstration can be found [here.](https://drive.google.com/file/d/18dDkOgW_vHvoqe4sad9TGroB5yceoKTG/view?usp=sharing)
--- server.js
+++ server.js
@@ -16,43 +16,47 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/api/notes", (req, res) => {
-  readFileAsync("./db/db.json", "utf-8").then(function(data) {
-    note = [].concat(JSON.parse(data))
+  readFileAsync("./db/db.json", "utf-8").then(function (data) {
+    note = [].concat(JSON.parse(data));
     res.json(note);
-  })
+  });
 });
 
 app.post("/api/notes", (req, res) => {
   const postingNotes = req.body;
-  readFileAsync("./db/db.json", "utf-8").then(function(data) {
-    const addingNotes = [].concat(JSON.parse(data));
-    postingNotes.id = addingNotes.length + 1 
-    addingNotes.push(postingNotes);
-    return addingNotes;
-  }).then(function(addingNotes) {
-    writeFileAsync("./db/db.json", JSON.stringify(addingNotes))
-    res.json(postingNotes);
-  })
+  readFileAsync("./db/db.json", "utf-8")
+    .then(function (data) {
+      const addingNotes = [].concat(JSON.parse(data));
+      postingNotes.id = addingNotes.length + 1;
+      addingNotes.push(postingNotes);
+      return addingNotes;
+    })
+    .then(function (addingNotes) {
+      writeFileAsync("./db/db.json", JSON.stringify(addingNotes));
+      res.json(postingNotes);
+    });
 });
 
-app.delete("/api/notes/:id", function(req, res) {
+app.delete("/api/notes/:id", function (req, res) {
   const toDelete = parseInt(req.params.id);
-  readFileAsync("./db/db.json", "utf-8").then(function(data) {
-    const notes = [].concat(JSON.parse(data));
-    const newNotes = []
-    for (let i = 0; i < notes.length; i++) {
-      if (toDelete !== notes[i].id) {
-        newNotes.push(notes[i])
+  readFileAsync("./db/db.json", "utf-8")
+    .then(function (data) {
+      const notes = [].concat(JSON.parse(data));
+      const newNotes = [];
+      for (let i = 0; i < notes.length; i++) {
+        if (toDelete !== notes[i].id) {
+          newNotes.push(notes[i]);
+        }
       }
-    }
-    return newNotes
-  }).then(function(notes) {
-    writeFileAsync("./db/db.json", JSON.stringify(notes))
-    res.send("Note Saved!");
-  })
-})
+      return newNotes;
+    })
+    .then(function (notes) {
+      writeFileAsync("./db/db.json", JSON.stringify(notes));
+      res.send("Note Saved!");
+    });
+});
 
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
